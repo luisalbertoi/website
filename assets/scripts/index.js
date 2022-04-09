88d6f42b05eaa8b65715694a4cff531ba87e15d4
Luis Alberto I <alberdj987@gmail.com>
2022-04-09 14:37:37 -0500

    update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,41 +1,19 @@
 const devices = [/Android/i, /webOS/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
 const isMobile = devices.some(x => navigator.userAgent.match(x));
 
-// container elements
-const pointer = document.getElementById('pointer');
-const motion = document.getElementById('motion');
-const nombre = document.getElementById('name');
-const form = document.getElementById('form');
-
 // animate elements
 const simplify = (arry, next, value) => arry.map((elem, i) => next(elem, value ? value : i));
 const animate = ({ style }, value) => style.transform = value;
 const await = (next, time) => setTimeout(() => next(), time);
 
-// pointer funcion
-const handler = ({ x, y, target }) => {
-  const { children } = pointer;
-  const exclude = [1, 2].map(x => children[x]);
-  const complete = [0, 1, 2].map(x => children[x]);
-
-  const translate = `translate(${x}px, ${y}px)`;
-
-  const { cursor } = window.getComputedStyle(target, null);
-
-  simplify(complete, animate, translate);
-
-  if (cursor === 'pointer') {
-    simplify(exclude, (elem, i) => animate(elem, `${translate} scale(${i > 0 ? 0 : 1})`));
-  } else {
-    simplify(exclude, (elem, i) => animate(elem, `${translate} scale(${i > 0 ? 1 : 0})`));
-  }
-}
-
 // motion elements
-function startMotion() {
+function motion(container, num) {
   const { body } = document;
-  const { children } = motion;
-  const { children: { length } } = motion;
+  for (let i = 0; i < num; i++) {
+    container.innerHTML += '<div></div>';
+  }
+  const { children } = container;
+  const { children: { length } } = container;
   const amount = new Array(length).fill('');
   const elements = amount.map((x, i) => children[i]);
 
@@ -49,9 +27,27 @@ function startMotion() {
   }, 2000);
 }
 
+// send email
+function contact(container) {
+  container.addEventListener('submit', env => {
+    env.preventDefault();
+
+    const formData = new FormData(form);
+
+    const data = {
+      email: 'alberdj987@gmail.com',
+      subject: formData.get('subject'),
+      message: formData.get('message')
+    };
+
+    document.location = `mailto:${data.email}?subject=${data.subject}&body=${data.message}`;
+
+  }, false);
+}
+
 // animate name
-function createName() {
-  const { children } = nombre;
+function name(container) {
+  const { children } = container;
 
   const print = (elem, arry) => {
     for (let i = 0; i < arry.length; i++) {
@@ -70,26 +66,28 @@ function createName() {
   simplify(letter, ({ num, value, time }) => await(() => print(children[num], value), time));
 }
 
-// send email
-form.addEventListener('submit', env => {
-  env.preventDefault();
+// pointer
+function pointer(container) {
+  const handler = ({ x, y, target }) => {
+    const { children } = container;
+    const exclude = [1, 2].map(x => children[x]);
+    const complete = [0, 1, 2].map(x => children[x]);
 
-  const formData = new FormData(form);
+    const translate = `translate(${x}px, ${y}px)`;
 
-  const data = {
-    email: 'alberdj987@gmail.com',
-    subject: formData.get('subject'),
-    message: formData.get('message')
-  };
+    const { cursor } = window.getComputedStyle(target, null);
 
-  document.location = `mailto:${data.email}?subject=${data.subject}&body=${data.message}`;
+    simplify(complete, animate, translate);
 
-}, false);
+    if (cursor === 'pointer') {
+      simplify(exclude, (elem, i) => animate(elem, `${translate} scale(${i > 0 ? 0 : 1})`));
+    } else {
+      simplify(exclude, (elem, i) => animate(elem, `${translate} scale(${i > 0 ? 1 : 0})`));
+    }
+  }
 
-// start
-createName();
-startMotion();
-if (!isMobile) {
-  pointer.style.display = 'initial';
-  document.addEventListener('mousemove', handler, true);
+  if (!isMobile) {
+    container.style.display = 'initial';
+    document.addEventListener('mousemove', handler, true);
+  }
 }
